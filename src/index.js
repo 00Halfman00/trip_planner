5c83dbcef06bab6f1b2c3c7c222c2c1ae652f181
00Halfman00 <00oscarsanchez00@gmail.com>
2020-08-13 10:53:16 -0500

    marker on page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,23 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiMDBvc2NhcnNhbmNoZXowMCIsImEiOiJja2Rzd3Q4M2gwM
 
 const map = new mapboxgl.Map({
     container: "map",
-    center: [-74.009, 40.705],
+    center: [-87.641, 41.895],
     zoom: 12,
     style: "mapbox://styles/mapbox/streets-v10"
-})
+});
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-87.641, 41.895]).addTo(map);
+
+// const markerDomEl = document.createElement("div"); // Create a new, detached DIV // creates a marker on map
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+// new mapboxgl.Marker(markerDomEl).setLngLat([-87.641, 41.895]).addTo(map); // [-87.641, 41.895] for Chicago
+
+
