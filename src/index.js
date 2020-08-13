57632217b9885c0e9a7ec64c4d2a6b5a0070b54a
00Halfman00 <00oscarsanchez00@gmail.com>
2020-08-13 09:54:33 -0500

    map in on screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from JavaScipt");
+//console.log("Hello from JavaScipt");
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiMDBvc2NhcnNhbmNoZXowMCIsImEiOiJja2Rzd3Q4M2gwMzdlMzNrNms0Zm5pMmp3In0.B-SX_Y5BqDU7coNV_1_zNA';
+
+const map = new mapboxgl.Map({
+    container: "map",
+    center: [-74.009, 40.705],
+    zoom: 12,
+    style: "mapbox://styles/mapbox/streets-v10"
+})
